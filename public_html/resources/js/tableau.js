8a78cd9f52bcdc44d5fe94fdf66f5d33fa2a3502
Lemaire Sophie <sophielemaire361@gmail.com>
2017-01-15 15:43:22 +0100

    exercice 6 fait

--- public_html/resources/js/tableau.js
+++ public_html/resources/js/tableau.js
@@ -1,23 +1,25 @@
 "use strict";
 
 function tableau(){
-    var tab= [4, 12, 6, 3];
+    var tab= [4,12,6,3];
     var somme=0;
     var valeur="";
     for(var i=0; i<=tab.length -1; i++){
+        somme = somme + tab[i];
         if (!isNaN(tab[i])){
-            if(i === 0){
-                nombres = ""+ tab[i]+"<br/>";
+            if(i===0){
+                valeur = ""+ tab[i]+"<br/>";
+            }
+            else {
+		valeur = valeur + "+" + tab[i] + "<br>";
             }
             
         }
-
-        valeur = valeur + tab[i]+ "<br>"
-        somme = somme + tab[i];
     }
-    document.getElementById("valeur").innerHTML =valeur;
-    document.getElementById("resultat").innerHTML=somme;
+   
 }
+document.getElementById("valeur").innerHTML=valeur;
+document.getElementById("resultat").innerHTML=somme;
 
 tableau();
 
