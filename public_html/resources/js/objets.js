bdfb9a39a79e766ad3ef791bc8381a25ae4e20c4
Lemaire Sophie <sophielemaire361@gmail.com>
2017-01-20 10:06:36 +0000

    modif exo7

--- public_html/resources/js/objets.js
+++ public_html/resources/js/objets.js
@@ -7,14 +7,24 @@ function Product(nom,description,poids,dimension,photo){
     this.description = description;
     this.poids = poids;
     this.dimension = dimension;
-    this.photo = photo;
+    this.photo = (src="../img/bonbon.png")
+    this.addFeature = function (nameFeature,descriptionFeature){
+       this[nameFeature] = descriptionFeature; 
+    };
     }
 
-var chocolat=new Product("kinder","chocolat blanc",300,30,"../img/bonbon.png");
+var chocolat=new Product("kinder","chocolat blanc",300,30,);
+debugger;
+chocolat.addFeature("quantite","cinq");
+chocolat.addFeature("quantite","quatre");
 
-document.getElementById("chocolat").innerHTML=chocolat.nom;
-document.getElementById("chocolat").innerHTML=chocolat.description;
-document.getElementById("chocolat").innerHTML=chocolat.poids;
-document.getElementById("chocolat").innerHTML=chocolat.dimension;
+
+
+document.getElementById("nom").innerHTML=chocolat.nom;
+document.getElementById("description").innerHTML=chocolat.description;
+document.getElementById("poids").innerHTML=chocolat.poids;
+document.getElementById("dimension").innerHTML=chocolat.dimension;
+document.getElementById("photo").appendChild(photo);
+
 
 
