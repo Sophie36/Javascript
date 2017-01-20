7504bddd2a85bbd35cf7b909b57c98b3e042c043
Lemaire Sophie <sophielemaire361@gmail.com>
2017-01-20 11:23:48 +0000

    ajout dans l'exercice 7

--- public_html/resources/js/objets.js
+++ public_html/resources/js/objets.js
@@ -2,29 +2,56 @@
 
 
 
-function Product(nom,description,poids,dimension,photo){
+function Product(reference,nom,description,poids,dimension,photo){
+    if (reference == undefined || reference == "" || reference == null){
+        this.reference = "01";
+    }
+    else {
+        this.reference = reference;
+    }
     this.nom = nom;
     this.description = description;
     this.poids = poids;
     this.dimension = dimension;
-    this.photo = (src="../img/bonbon.png")
+    this.photo = photo;
+    this.subProduct = [];
     this.addFeature = function (nameFeature,descriptionFeature){
        this[nameFeature] = descriptionFeature; 
     };
+    this.addSubProduct = function(param){
+        if(param instanceof Product){
+            this.subProduct.push(param);
+        }
+    } 
     }
+    
 
-var chocolat=new Product("kinder","chocolat blanc",300,30,);
+
+var chocolat=new Product("","kinder","chocolat blanc",300,30,"../img/bonbon.png");
 debugger;
 chocolat.addFeature("quantite","cinq");
-chocolat.addFeature("quantite","quatre");
+
+var gateau=new Product("","cake","fruit",150,510);
+gateau.addFeature("quantite","quatre");
+
+chocolat.addSubProduct (gateau);
+
+function initialiseProductFiche(param) {
+    debugger;
+    Object.keys(param).forEach(function (key) {
+        debugger;
+        if ("photo" != key) {
+            var text = undefined != param[key] ? param[key] : "";
+            document.getElementById(key).innerHTML = text;
+        } else {
+           document.getElementById(key).src = param[key];    
+        }
+    });
+}
 
 
 
-document.getElementById("nom").innerHTML=chocolat.nom;
-document.getElementById("description").innerHTML=chocolat.description;
-document.getElementById("poids").innerHTML=chocolat.poids;
-document.getElementById("dimension").innerHTML=chocolat.dimension;
-document.getElementById("photo").appendChild(photo);
 
 
+initialiseProductFiche(chocolat);
 
